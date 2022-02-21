fc05784afce5b184913f34d9b790c73c93ffdd90
Prismo <prismo@minetest.ca>
2022-02-21 06:18:20 +0000

    inital commit and complete rewrite of chat_censor

---
+++ init.lua
@@ -0,0 +1,225 @@
+-- load modpath
+local MP = minetest.get_modpath(minetest.get_current_modname())
+
+local censor = {
+
+    -- some settings for default behaviour of mod
+    kick_enable = minetest.settings:get_bool('censor.kick_enable',true) or true,
+    warn_enable = minetest.settings:get_bool('censor.warn_enable',true) or true,
+    caps_enable = minetest.settings:get_bool('censor.caps_enable',true) or true,
+    name_enable = minetest.settings:get_bool('censor.name_enable',true) or true,
+    warn_cost = minetest.settings:get('censor.warn_cost') or 3,
+    caps_cost = minetest.settings:get('censor.caps_cost') or 2,
+    violation_limit = minetest.settings:get('censor.violation_limit') or 12,
+    caps_limit = minetest.settings:get('censor.violation_limit') or 50,
+    -- substr_match is quite strict, either drop its support or allow some way of blacklisting
+    substr_match = minetest.settings:get_bool('censor.substr_match',false) or false,
+    -- fun mode refers to substituting a bad word with a fun word
+    fun_mode = minetest.settings:get_bool('censor.fun_mode',true) or true,
+    -- stores violation of online players
+    violations = {}, fun_words={}, bad_words = {}, bad_en_words = {},whitelist = {},
+    lang = {"en","zh","fil","fr","de","hi","ja","pl","ru","es","th"},
+}
+local colors = {
+    warn = "#ff0000",
+    info = "#ffff00"
+}
+
+-- check if file exists
+local function file_exists(file)
+    local f = io.open(file,"rb")
+    if f then f:close() end
+    return f~=nil
+end
+
+-- populate bad_list and bad_en_list
+local dirname = MP .. "/wordlist"
+for _,f in pairs(censor.lang) do
+    local fname = dirname.. "/" .. f
+    if file_exists(fname) then
+        -- append file content to the dictionary bad_words
+        for line in io.lines(fname) do
+            if f == "en" then
+                table.insert(censor.bad_en_words,line)
+            end
+            table.insert(censor.bad_words,line)
+        end
+    end
+end
+
+-- populate fun list
+local fun_file = dirname .. "/fun"
+if file_exists(fun_file) then
+    for line in io.lines(fun_file) do
+        table.insert(censor.fun_words, line)
+    end
+end
+
+-- onprejoin changes by mod
+minetest.register_on_prejoinplayer(function(name)
+    for _,v in pairs(censor.bad_words) do
+        if not censor.whitelist[name] and string.find(string.lower(name),string.lower(v)) then
+            -- print on console
+            print("[censor]: Blocked user " ..name.. " from joining because their name contains word : " .. v)
+            return "Your name contained a blocked word :"..v..", Please try again with better name."
+        end
+    end
+end)
+
+-- onjoin changes by mod
+minetest.register_on_joinplayer(function(player)
+    -- set violations for each new player as 0
+    censor.violations[player:get_player_name()] = 0
+end)
+
+-- censor warn
+function censor.warn(name)
+    if not censor.warn_enable then return end
+    local vbalance = ""
+    if censor.violations[name] and censor.kick_enable then
+        vbalance = vbalance ..  "Violation Status: ".. censor.violations[name] .. "/"..censor.violation_limit
+    end
+    local warnmsg = minetest.colorize(
+        colors["warn"],"Your last message will be reported to server staff! " .. vbalance )
+    minetest.chat_send_player(name,warnmsg)
+end
+
+-- censor caps check a message
+function censor.caps(name,msg)
+
+    if not censor.caps_enable then return end
+    if msg:len() > 256 then
+        -- Add 3 cost for long messages
+        censor.violations[name] = (censor.violations[name] or 0) + censor.caps_cost
+        minetest.chat_send_player(name,minetest.colorize(colors["info"],"Long messages are considered as SPAM."))
+        censor.warn(name)
+    end
+
+    -- no need to check short messages for caps
+    if msg:len() > 8 then
+        local caps = 0
+        for i = 1,#msg do
+            local c = msg:sub(i,i)
+            -- replace everything that isn't a letter with smaller ones
+            c = c:gsub('%A','a')
+            if c == c:upper() then
+                caps = caps + 1
+            end
+        end
+        -- now we know caps
+        if (caps*100/msg:len()) >= censor.caps_limit then
+            -- Add some cost
+            censor.violations[name] = (censor.violations[name] or 0) + censor.caps_cost
+            minetest.chat_send_player(name,minetest.colorize(colors["info"],"CAPS ALERT"))
+            censor.warn(name)
+        end
+    end
+end
+
+-- kick on violation limit exceded
+function censor.kick(name)
+    -- check if enabled kick
+    if not censor.kick_enable then return false end
+    -- local player = minetest.get_player_by_name(name)
+    if minetest.check_player_privs(name,"server") then
+        return false
+    end
+    if censor.violations[name] >= censor.violation_limit then
+        minetest.kick_player(name, "Violations Limits Excedded")
+        minetest.chat_send_all(name .. minetest.colorize(colors["warn"]," has been kicked due to violations limits."))
+        print("[censor]: " .. name .. " was kicked due to violations limits.")
+        return true
+    end
+end
+
+-- fixing message
+function censor.fix_message(name,message)
+    -- Censor Code
+    -- "Lua is sexy" -> "Lua is ****"
+    local mes = ""
+    for w in message:gmatch("%S+") do
+        for _,v in pairs(censor.bad_words) do
+            if v:len() >= 4 then
+                -- do contains operation
+            if string.find(string.lower(w), string.lower(v)) then
+                local pat = string.rep("*",v:len())
+                if censor.fun_mode then
+                    pat = censor.fun_words[math.random(1,#censor.fun_words)]
+                end
+                w = pat
+            end
+            else
+                -- do match operation
+            if string.lower(w) == string.lower(v) then
+                local pat = string.rep("*",v:len())
+                if censor.fun_mode then
+                    pat = censor.fun_words[math.random(1,#censor.fun_words)]
+                end
+                w = pat
+            end
+            end
+        end
+        mes = mes .. w .. " "
+    end
+    -- remove last " "
+    mes = mes:sub(1,-2)
+
+    --Censor Code v2
+    -- "Lua is sexy" -> "Lua is ***y"
+    --[[
+    local mes = string.lower(message)
+    for _,v in pairs(bad_words) do
+        -- Apply fun word substitution
+        local pat = string.rep("*",v:len())
+        if censor.fun_mode then
+            pat = censor.fun_words[math.random(1,#censor.fun_words)] .. " "
+        end
+        mes = mes:gsub(v,pat)
+    end
+    --]]
+
+    -- warn the offender
+    if string.lower(mes) ~= string.lower(message) then
+        print("[censor]: ".. name .. " sent censored message :"..message)
+        censor.violations[name] = (censor.violations[name] or 0) + censor.warn_cost
+        censor.warn(name)
+    end
+    return mes
+end
+
+-- main msg checking function
+minetest.register_on_chat_message(function(name, message)
+
+    -- Before sending message check shout privs
+    if not minetest.check_player_privs(name, "shout") then
+        return
+    end
+
+    censor.caps(name,message)
+    -- main censoring function
+    -- #########
+    message = censor.fix_message(name,message)
+
+    -- if player kicked then no need to broadcast their message
+    if censor.kick(name) then
+        return true
+    end
+
+    -- send the manipulated message to everyone
+    for _,player in pairs(minetest.get_connected_players()) do
+        -- reciever is everyone
+        local rname = player:get_player_name()
+        local color = "#ffffff"
+
+        -- add feature of mention highlight
+        -- #######
+        local send = name .. ": ".. minetest.colorize(color,message)
+        minetest.chat_send_player(rname,send)
+    end
+
+    -- return true to override default chat functionality
+    return true
+end)
+
+-- console print mod status
+print("[censor] OK")
